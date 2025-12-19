ed5c77a4eca61751f07a85ebb54f32b960519539
Luca Iovio <luca.iovio@gmail.com>
2025-12-19 16:14:20 +0100

    http server configs

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -66,10 +66,14 @@ func main() {
 	}
 
 	srv := &http.Server{
-		Addr:      *addr,
-		Handler:   app.routes(),
-		ErrorLog:  slog.NewLogLogger(logger.Handler(), slog.LevelError),
-		TLSConfig: tlsConfig,
+		Addr:           *addr,
+		MaxHeaderBytes: 524288,
+		Handler:        app.routes(),
+		ErrorLog:       slog.NewLogLogger(logger.Handler(), slog.LevelError),
+		TLSConfig:      tlsConfig,
+		IdleTimeout:    time.Minute,
+		ReadTimeout:    5 * time.Second,
+		WriteTimeout:   10 * time.Second,
 	}
 
 	logger.Info("starting server", "addr", srv.Addr)
