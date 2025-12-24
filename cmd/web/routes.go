f410e488e7e430a46081b2369ab2a511c4a629e5
Luca Iovio <luca.iovio@gmail.com>
2025-12-24 12:28:20 +0100

    embed

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"lucaiovio_snippetbox/ui"
 	"net/http"
 
 	"github.com/justinas/alice"
@@ -9,8 +10,7 @@ import (
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+	mux.Handle("GET /static/", http.FileServerFS(ui.Files))
 
 	dynamic := alice.New(app.sessionManager.LoadAndSave, preventCSRF, app.authenticate)
 
